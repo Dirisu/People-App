13e2edc3eab5b68e972807be7378762b95579bde
Dirisu <dirisumarvellous94@gmail.com>
2023-09-22 07:02:35 +0100

    set up project structure and launch screen for project

---
+++ People/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  People
+//
+//  Created by Marvellous Dirisu on 22/09/2023.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
