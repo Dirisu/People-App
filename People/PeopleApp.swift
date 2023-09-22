58ebcfd183bafc1202cce0bad4663ed142b9207a
Dirisu <dirisumarvellous94@gmail.com>
2023-09-22 05:50:54 +0100

    Initial Commit

---
+++ People/PeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleApp.swift
+//  People
+//
+//  Created by Marvellous Dirisu on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
